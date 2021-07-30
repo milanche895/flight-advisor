55ce68da8a2d9a07dfb8e095b017bc850b83733c
milanche895 <milanstojanovic895@gmail.com>
2021-07-30 23:08:01 +0200

    test

---
+++ src/main/java/com/test/htec/FlightAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.test.htec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightAdvisorApplication.class, args);
+	}
+
+}
